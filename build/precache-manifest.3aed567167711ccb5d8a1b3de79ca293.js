a10a99cdd5671925d845b9b84a80f53074986c9b
frongfrank17 <50213997+frongfrank17@users.noreply.github.com>
2020-05-24 20:20:57 +0700

    first commit

---
+++ build/precache-manifest.3aed567167711ccb5d8a1b3de79ca293.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7006baf8ceaf682446ef22be79151b03",
+    "url": "/index.html"
+  },
+  {
+    "revision": "350b5804e7feac542116",
+    "url": "/static/css/2.4af70dfe.chunk.css"
+  },
+  {
+    "revision": "6f2119c3c07cd475675a",
+    "url": "/static/css/main.8c75cbd4.chunk.css"
+  },
+  {
+    "revision": "350b5804e7feac542116",
+    "url": "/static/js/2.0615aa6b.chunk.js"
+  },
+  {
+    "revision": "50ebf9df8e516f397c4565cccb79e50d",
+    "url": "/static/js/2.0615aa6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f2119c3c07cd475675a",
+    "url": "/static/js/main.2da32eca.chunk.js"
+  },
+  {
+    "revision": "e3617af037283fc3fc0c",
+    "url": "/static/js/runtime-main.f4b46fba.js"
+  }
+]);
